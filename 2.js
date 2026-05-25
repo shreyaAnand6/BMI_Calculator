cf8ecfbb3c42ed3a694ab78e738f3a967cd087a4
Shreya Anand <shreya001anand@gmail.com>
2026-05-25 13:03:57 +0530

    Refactor BMI calculation and add weight status

--- 2.js
+++ 2.js
@@ -6,6 +6,7 @@ form.addEventListener('submit',function(e){
 
     const ht=document.querySelector(".ht");
     const wt= parseInt(document.querySelector(".wt").value);
+    let ans;
 
     console.log(ht,typeof ht.value);
     h=parseInt(ht.value)
@@ -15,9 +16,16 @@ form.addEventListener('submit',function(e){
     if (h<0 || wt<0){
         res.innerText="Please give Valid height and Weight"
     }else{
-        res.innerText=`Your BMI is ${wt/(h*h/10000).toFixed(3)}`;
+        ans=(wt/(h*h/10000))
+        const text=`Your BMI is ${ans.toFixed(3)}`;
+        if (ans<18.6){
+            res.innerText=`${text}\nUNDER WEIGHT`
+        }else if(ans<=24.9){
+            res.innerText=`${text}\nNORMAL`
+        }else{
+            res.innerText=`${text}\nOVERWEIGHT`
+        }
     }
     
-
 })
 
